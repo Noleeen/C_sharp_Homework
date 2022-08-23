3295d752ab04778ad9a0544dd07d00ce59e962e2
Ivan Gerasimenko <noleen@ya.ru>
2022-08-23 16:06:21 +0300

    task6 task 8

--- hw 1/Program.cs
+++ hw 1/Program.cs
@@ -17,7 +17,7 @@ Console.WriteLine("max = " + max);
 // -----------------------------------------------------------------------------------
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
-
+/*
 Console.WriteLine("enter first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -36,3 +36,32 @@ if(num3 > max)
     max = num3;
 
 Console.WriteLine("max = " + max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.WriteLine("enter your number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+    Console.WriteLine("this is even number");
+
+else 
+    Console.WriteLine("this is odd number");
+    */
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("enter your number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count < num)
+    {
+        if(count % 2 == 0)
+            {
+                Console.WriteLine(count);
+            }
+        count++;
+    }
