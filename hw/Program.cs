16aacb043734b16efa5b3c0668f6095a9e54b397
Ivan Gerasimenko <noleen@ya.ru>
2022-08-25 20:11:57 +0300

    homework2 task10

---
+++ hw/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+void SecondDigit(int num)
+{
+    int des = num % 100;
+    int result = des / 10;
+    Console.WriteLine(result); 
+}
+
+Console.WriteLine("enter threedigit number");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+SecondDigit(userNumber);
+
