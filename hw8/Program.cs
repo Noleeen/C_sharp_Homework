25060281a93a565fb30e43372bcee2545bbb5a8a
Ivan Gerasimenko <noleen@ya.ru>
2022-09-19 13:11:45 +0300

    homework 8 task 54 56 58 60 62

--- hw8/Program.cs
+++ hw8/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
 // которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
-/*
+
 int[,] UserMatrix()
 {
     Console.Write("input number of rows ");
@@ -61,11 +61,11 @@ int[,] myArray = UserMatrix();
 ShowMatrix(myArray);
 SortMatrix(myArray);
 ShowMatrix(myArray);
-*/
+
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
 // которая будет находить строку с наименьшей суммой элементов.
 
-/*
+
 
 int[,] UserMatrix()
 {
@@ -123,7 +123,7 @@ void SumRows(int[,] matrix)
 int[,] myArray = UserMatrix();
 ShowMatrix(myArray);
 SumRows(myArray);
-*/
+
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет 
 // находить произведение двух матриц.
@@ -187,3 +187,170 @@ ShowMatrix(myArray);
 ShowMatrix(myArray2);
 int[,] multMatr = MultipliMatrix(myArray, myArray2);
 ShowMatrix(multMatr);
+
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+
+int[,,] UserMatrix3d()
+{
+    Console.Write("input number of rows ");
+    int d1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("input number of columns ");
+    int d2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("input number of 3d ");
+    int d3 = Convert.ToInt32(Console.ReadLine());
+
+    int[,,] matrix = new int[d1, d2, d3];
+    for (int i = 0; i < d1; i++)
+        for (int j = 0; j < d2; j++)
+            for (int k = 0; k < d3; k++)
+                matrix[i, j, k] = new Random().Next(10, 65);
+    return matrix;
+}
+
+void DontRepeat(int[,,] m)
+{
+    int max = m[0, 0, 0];
+    for (int i = 0; i < m.GetLength(0); i++)
+        for (int j = 0; j < m.GetLength(1); j++)
+            for (int k = 0; k < m.GetLength(2); k++)
+                if (max < m[i, j, k])
+                    max = m[i, j, k];
+    int temp = m[0, 0, 0];
+    int current = 1;
+    for (int i = 0; i < m.GetLength(0); i++)
+        for (int j = 0; j < m.GetLength(1); j++)
+            for (int k = 0; k < m.GetLength(2); k++)
+            {
+                for (int x = 0; x < m.GetLength(0); x++)
+                    for (int y = 0; y < m.GetLength(1); y++)
+                        for (int z = 0; z < m.GetLength(2); z++)
+                        {
+                            if (x == i && y == j && z == k) continue;
+                            if (m[x, y, z] == m[i, j, k])
+                            {
+                                m[x, y, z] = max + current;
+                                current++;
+                            }
+
+                            // вот это второй вариант без поиска Макса, в нём есть нюнс, что 
+                            // диапазон чисел должен быть на много больше элементов массива, 
+                            // иначе будет долго подбирать 
+                            // случайные неповторяющиеся числа...
+                            
+                            // if (x == i && y == j && z == k) continue;
+                            // if (m[x, y, z] == m[i, j, k])
+                            // {
+                            //     int end = 1;
+                            //     while( end == 1)
+                            //     {
+                            //         m[x, y, z] = new Random().Next(10,65);
+                            //         for (int q = x; q >= 0; q--)
+                            //             for (int w = y; w >= 0; w--)
+                            //                 for (int e = z; e >= 0; e--)
+                            //                 {
+                            //                     if (x == q && y == w && z == e) continue;
+                            //                     if (m[x, y, z] == m[q, w, z])
+                            //                         end = 1;
+                            //                     else end = 0;
+                            //                 }
+                            //     }
+                            // }
+                            
+                        }
+            }
+
+
+}
+
+void ShowMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            for (int j = 0; j < matrix.GetLength(2); j++)
+                Console.Write($"{matrix[i, k, j]} ({i},{k},{j})  ");
+            Console.WriteLine();
+        }
+    Console.WriteLine();
+}
+
+int[,,] my3dArray = UserMatrix3d();
+ShowMatrix(my3dArray);
+
+DontRepeat(my3dArray);
+
+ShowMatrix(my3dArray);
+
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+
+int[,] UserMatrix()
+{
+    Console.Write("input number of rows ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    int[,] matrix = new int[size, size];
+
+    int p = 0;
+    int current = 1;
+    for (int n = size; n > size / 2; n--)
+    {
+        int i = p;
+        for (int j = p; j < n; j++)
+        {
+            if(matrix[i, j] != 0) break;
+            matrix[i, j] = current;
+            current++;
+            if (j == n - 1)
+            {
+                for (i = p + 1; i < n; i++)
+                {
+                    if(matrix[i, j] != 0) break;
+                    matrix[i, j] = current;
+                    current++;
+                    if (i == n - 1)
+                    {
+                        for (j = n - 2; j >= p; j--)
+                        {
+                            if(matrix[i, j] != 0) break;
+                            matrix[i, j] = current;
+                            current++;
+                            if (j == p)
+                            {
+                                for (i = n - 2; i > 0 + p; i--)
+                                {
+                                    matrix[i, j] = current;
+                                    current++;
+                                }
+                            }
+                        }
+                    }
+                    Console.WriteLine("вот ещё работает");
+                }Console.WriteLine("вот тут стопорится((");
+            }
+        }
+        p++;
+
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + "   ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] myArray = UserMatrix();
+ShowMatrix(myArray);
+
